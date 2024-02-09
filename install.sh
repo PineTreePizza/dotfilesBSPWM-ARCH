71e72cb94d74f769f2ce040f9281eee9b98239d6
Anton Kostochka <77921296+PineTreePizza@users.noreply.github.com>
2024-02-09 10:37:23 +0300

    Added an install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+mkdir $HOME/.config/
+mv alacritty $HOME/.config/
+mv bspwm $HOME/.config/
+mv sxhkd $HOME/.config/
+mv polybar $HOME/.config/
+mv rofi $HOME/.config/
+mv picom.conf $HOME/.config/
+mv dunst $HOME/.config/
+mv flameshot $HOME/.config/
+mv nvim $HOME/.config/
+mv .gtkrc-2.0.mine $HOME/
+mv gtk-2.0/ $HOME/.config
+mv gtk-3.0 $HOME/.config
+mv gtk-4.0/ $HOME/.config
